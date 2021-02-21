ae2b8036542021802ff1a2699acb55ffef3c61c4
nakatievent <nakatievent@gmail.com>
2021-02-21 12:46:05 +0900

    テストデータの挿入とindexアクションの実装

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class UserController extends Controller
 {
     /**
@@ -13,7 +15,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+      $users = User::all();
+      return $users;//
     }
 
     /**
