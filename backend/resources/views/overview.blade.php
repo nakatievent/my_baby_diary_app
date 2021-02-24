8bdfbf31c16322cbf5f67d23a27366bfa096c33c
nakatievent <nakatievent@gmail.com>
2021-02-24 12:07:36 +0900

    overview.blade.phpの作成と編集

---
+++ backend/resources/views/overview.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="overview.css">
+
+  <title>babydiaryとは？</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h1 class="text-center">babydiaryとは？</h1>
+        <p class="text-center">子供との思い出を写真や日記に残すことができるアプリです。時々思い出を振り返る際にも是非ご活用下さい。</p>
+      </div>
+        <h2 class="text-center">使い方</h2>
+      <div class="col-md-6 mx-auto">
+        <img src="img/4212014_s.jpg" class="img-fluid rounded mx-auto d-block border border-4" alt="...">
+        <h3 class="text-center">①子供との思い出を残す時に使う</h3>
+        <p class="text-center">どこかに出かけた時やご飯を食べている時、遊んだ時など、子供と過ごした時間は一瞬一瞬が素晴らしい瞬間です。その大切な思い出を日記に残してみましょう。</p>
+      </div>
+      <div class="col-md-6 mx-auto">
+        <img src="img/4212014_s.jpg" class="img-fluid rounded mx-auto d-block border border-4" alt="...">
+        <h3 class="text-center">②思い出を振り返りたい時に使う</h3>
+        <p class="text-center">思い出を振り返る時にも使えます。今はスマホで気軽に写真を振り返ることもできますが、その時の感情や思ったことを振り返り懐かしむこともできます。。また、保育園での先生とのやりとりを残してみることもオススメです♪</p>
+      </div>
+      <!-- </div> -->
+    </div>
+  </div>
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
