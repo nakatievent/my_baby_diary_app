11b13f1ca97be962f9630546ecbb5b5ed5068985
nakatievent <nakatievent@gmail.com>
2021-02-24 16:08:43 +0900

    ログインページ（login.blade.php）の編集

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -1,10 +1,27 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="login.css">
+
+  <title>ログインページの編集</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+
+    </div>
+
+    <div class="row">
+      <div class="col-md-6 mx-auto border border-dark border-3 p-5 position-absolute top-50 start-50 translate-middle">
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -12,45 +29,60 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('login') }}">
+        <div class="fw-bolder text-center">
+          <h1 class="fw-bolder fs-2">採用担当者様<br>専用ログインフォーム</h1>
+          <p></p>
+          <p>下記の「専用ログイン」ボタンを押して頂くと、すぐにログインが可能です。</p>
+        </div>
+
+        <form class="col-lg-8 mx-auto" method="POST" action="{{ route('login') }}">
             @csrf
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <!-- メールアドレス -->
+            <div class="form-group">
+                <label for="email" class="form-label">メールアドレス</label>
+                <input id="email" class="form-control" type="email" name="email" aria-describedby="emailHelp" :value="old('email')" required autofocus/>
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+            <br>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+            <!-- パスワード -->
+            <div class="form-group">
+                <label for="password" class="form-label">パスワード</label>
+                <input id="password" class="form-control" type="password" name="password"
+                       required autocomplete="current-password">
             </div>
+
+            <br>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
+            <div class="form-check">
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+                    <label class="form-check-label" for="remember_me">{{ __('ログイン状態を維持する') }}</label>
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-center mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ ('パスワードを忘れましたか?') }}
                     </a>
                 @endif
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
+                　<button type="submit" class="btn btn-primary">専用ログイン</button>
+                </button>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+  </div>
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
