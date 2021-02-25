a79a9ff35e897d9a8a3f98483014fa46518d0fa4
nakatievent <nakatievent@gmail.com>
2021-02-25 20:01:47 +0900

    usersテーブルのpictureカラムとmy_introductionカラムのnull許容

---
+++ backend/database/migrations/2021_02_25_025133_change_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('picture')->nullable()->change();
+            $table->text('my_introduction')->nullable()->change();
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
