02ce9b1f07fd342140b9c768a1fd32b99adf2044
nakatievent <nakatievent@gmail.com>
2021-02-28 18:24:40 +0900

    index.blade.phpとshow.blade.phpの編集

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,37 +1,48 @@
-{{-- layoutsフォルダのapp.blade.phpを継承 --}}
-@extends('layouts.app')
+{{-- layoutsフォルダのapp.blade.phpを継承 --}} 
+@extends('layouts.app') 
 
-{{-- @yield('title')にテンプレートごとの値を代入 --}}
-@section('title', '投稿一覧')
+{{-- @yield('title')にテンプレートごとの値を代入 --}} 
+@section('title','投稿一覧')
 
-{{-- app.blade.phpの@yield('content')に以下のレイアウトを代入 --}}
+{{-- app.blade.phpの@yield('content')に以下のレイアウトを代入 --}} 
 @section('content')
+
+<div>
+    <a href="/posts/create">新規作成</a>
+</div>
 
 <!-- <ここからは日記投稿画面> -->
 <div class="row row-cols-1 row-cols-xl-3 g-4">
-  @foreach ($posts as $post)
-  <div class="col">
-    <div class="card h-100"> <img src="image/4212014_s.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">{{$post->title}}</h5>
-        <p class="card-text">{{$post->diary}}</p>
-        <a href="/posts/{{$post->id}}">詳細を表示</a>
-      </div>
-      <div class="card-footer"> <small class="text-muted">Last updated 3 mins ago</small> </div>
+    @foreach ($posts as $post)
+    <div class="col">
+        <div class="card h-100"> <img src="image/4212014_s.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{$post->title}}</h5>
+                <p class="card-text">{{$post->diary}}</p>
+                <a href="/posts/{{$post->id}}">詳細を表示</a>
+                <a href="/posts/{{$post->id}}/edit">編集する</a>
+                <form action="/posts/{{$post->id}}" method="post">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="_method" value="delete">
+                    <input type="submit" name="" value="削除する">
+                </form>
+                {{-- <a href="/posts/{{$post->id}}">削除する</a> --}}
+            </div>
+            <div class="card-footer"> <small class="text-muted">Last updated 3 mins ago</small> </div>
+        </div>
     </div>
-  </div>
-  @endforeach
+    @endforeach
 </div>
 <br>
 <!-- <ここからは日記投稿終了> -->
 
 <!-- ＜ここからはページネーション＞ -->
 <nav aria-label="Search results pages">
-  <ul class="pagination pagination-md justify-content-center">
-    <li class="page-item active" aria-current="page"> <span class="page-link">1</span> </li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-  </ul>
+    <ul class="pagination pagination-md justify-content-center">
+        <li class="page-item active" aria-current="page"> <span class="page-link">1</span> </li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+    </ul>
 </nav>
 <!-- ＜ページネーション終了＞ -->
 @endsection
--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -9,5 +9,11 @@
   <h1>{{$post->title}}</h1>
   <p>{{$post->diary}}</p>
   <br><br>
+  <a href="/posts/{{$post->id}}/edit">編集する</a>
+  <form action="/posts/{{$post->id}}" method="post">
+      {{ csrf_field() }}
+      <input type="hidden" name="_method" value="delete">
+      <input type="submit" name="" value="削除する">
+    </form>
   <a href="/posts">一覧に戻る</a>
 @endsection
