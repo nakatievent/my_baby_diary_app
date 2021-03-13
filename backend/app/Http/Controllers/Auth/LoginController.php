202a4b023d9a84fd8919ba03a29dea4dcf3c48e6
nakatievent <nakatievent@gmail.com>
2021-03-13 16:52:58 +0900

    ゲストログイン機能をコントローラーに追記する

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -18,4 +19,18 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+    public function guestLogin()
+    {
+        $email = 'example@gmail.com';
+        $password = 'password';
+    
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            return redirect()->route('posts.index');
+        }
+    
+        return redirect('/');
+    }
+
 }
