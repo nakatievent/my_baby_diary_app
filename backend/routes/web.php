5044553a949f948c068e27ec8b91acdc27cdc1ce
nakatievent <nakatievent@gmail.com>
2021-03-13 16:49:22 +0900

    ルーティングの編集（お気に入り機能とゲストログイン機能のルーティング追加）

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,20 +15,22 @@ Route::get('/overview', function () {
     return view('overview');
 });
 
-Route::resources([
-    'users' => UserController::class,
-]);
+// ゲストログイン機能
+Route::get('/login/guest', [App\Http\Controllers\Auth\LoginController::class, 'guestLogin']);
 
-// ログインしていないとPostに関するRESTfulな機能を使えずにログイン画面に戻る
+// ログインしていないと下記の機能が使えない
 Route::middleware(['auth']) -> group(function () {
-    Route::get('/posts/favorites', [App\Http\Controllers\PostController::class, 'favorites'])->name('posts.favorite');
+
+    // ログインしていないとPostに関するRESTfulな機能を使えずにログイン画面に戻る
     Route::resources([
         'posts' => PostController::class,
     ]);
+    // ログインしていないとお気に入りに追加した画像が見えないようにする設定
+    Route::get('/posts/favorites', [App\Http\Controllers\PostController::class, 'favorites'])->name('posts.favorite');
+    // ログインしていないとお気に入りに追加することができない設定
     Route::get('/posts/add_favorite/{id}', [App\Http\Controllers\PostController::class, 'add_favorite']);
-
 
 });
 
-Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Auth::routes();
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
