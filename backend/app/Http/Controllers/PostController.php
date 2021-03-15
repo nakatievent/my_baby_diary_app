09dcb650d52a15a85b4dfac7f901760cbbec67ea
nakatievent <nakatievent@gmail.com>
2021-03-15 22:07:35 +0900

    Postコントローラーの微修正

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -36,7 +36,7 @@ class PostController extends Controller
     {
       $user = Auth::user();
       $posts = $user->favorite_posts()->get();
-      dd($posts);
+      return view('posts.favorites');
     }
 
 
